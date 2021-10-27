1fbdc14fb9f932ff9d4b9ca095eb15afde1a6234
unknown <sajeel.ahmed@protonmail.com>
2021-10-27 21:34:18 +0500

    :tada: Complete OnGoing Trip

--- components/Parent/OnGoingTrip.js
+++ components/Parent/OnGoingTrip.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { StyleSheet, Image, View, Text } from 'react-native'
+import { StyleSheet, Image, View, Text, TouchableOpacity } from 'react-native'
 
 import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
+
+import onGoingTripPic from '../../assets/ongoing.png'
 
 export default function OnGoingTrip() {
   return (
@@ -10,7 +12,15 @@ export default function OnGoingTrip() {
       <Header back={true} />
       <View style={styles.contentContainer}>
         <Text style={styles.heading}>David's Trip</Text>
-        <View style={styles.ongoingBox}></View>
+        <View style={styles.ongoingBox}>
+          <Image
+            source={onGoingTripPic}
+            style={{ width: '87%', height: '87%' }}
+          ></Image>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Track</Text>
+        </TouchableOpacity>
       </View>
       <Footer dashboard={true} />
     </View>
@@ -20,6 +30,7 @@ export default function OnGoingTrip() {
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
+    maxHeight: '85%',
     flexDirection: 'column',
     justifyContent: 'space-between',
     height: '100%',
@@ -53,5 +64,19 @@ const styles = StyleSheet.create({
     elevation: 6,
     marginTop: 30,
     borderRadius: 10,
+  },
+  button: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    width: '70%',
+    marginTop: '10%',
+    alignItems: 'center',
+    backgroundColor: '#2B88C6',
+    padding: '4%',
+    borderRadius: 100,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
   },
 })

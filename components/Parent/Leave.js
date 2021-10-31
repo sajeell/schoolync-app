8e6b92e61c27b7625f1f0798e51fd8d573536b35
unknown <sajeel.ahmed@protonmail.com>
2021-10-31 16:54:29 +0500

    :tada: Complete Application Page

--- components/Parent/Leave.js
+++ components/Parent/Leave.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react'
-import { StyleSheet, Image, View, Text, TouchableOpacity } from 'react-native'
+import {
+  StyleSheet,
+  Image,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native'
 
 import { CheckBox } from 'react-native-elements'
 
@@ -14,48 +21,55 @@ export default function Leave() {
       <Header back={true} />
       <View style={styles.contentContainer}>
         <Text style={styles.heading}>Leave Application</Text>
-        <View style={styles.contentContainer}>
-          <Text style={styles.mainText}>No ride for</Text>
-          <View style={styles.checkBoxContainer}>
-            <CheckBox
-              checked={toggleMorningCheckBox}
-              onPress={() => {
-                setToggleMorningCheckBox(!toggleMorningCheckBox)
-              }}
-              containerStyle={{
-                backgroundColor: 'transparent',
-                borderWidth: 0,
-                padding: 0,
-                margin: 0,
-                marginLeft: 0,
-                fontFamily: 'Nunito_400Regular',
-              }}
-            />
-            <Text style={styles.checkBoxText}>Morning Shift</Text>
-          </View>
-          <View style={styles.checkBoxContainer}>
-            <CheckBox
-              checked={toggleEveningCheckBox}
-              onPress={() => {
-                setToggleEveningCheckBox(!toggleEveningCheckBox)
-              }}
-              containerStyle={{
-                backgroundColor: 'transparent',
-                borderWidth: 0,
-                padding: 0,
-                margin: 0,
-                marginLeft: 0,
-                fontFamily: 'Nunito_400Regular',
-              }}
-            />
-            <Text style={styles.checkBoxText}>Afternoon Shift</Text>
-          </View>
+
+        <Text style={styles.mainText}>
+          No ride for
+          <Text style={{ fontFamily: 'Nunito_700Bold' }}>
+            {' '}
+            12th October, 2021
+          </Text>
+        </Text>
+        <View style={styles.checkBoxContainer}>
+          <CheckBox
+            checked={toggleMorningCheckBox}
+            onPress={() => {
+              setToggleMorningCheckBox(!toggleMorningCheckBox)
+            }}
+            containerStyle={{
+              backgroundColor: 'transparent',
+              borderWidth: 0,
+              padding: 0,
+              margin: 0,
+              marginLeft: 0,
+            }}
+          />
+          <Text style={styles.checkBoxText}>Morning Shift</Text>
         </View>
+        <View style={styles.checkBoxContainer}>
+          <CheckBox
+            checked={toggleEveningCheckBox}
+            onPress={() => {
+              setToggleEveningCheckBox(!toggleEveningCheckBox)
+            }}
+            containerStyle={{
+              backgroundColor: 'transparent',
+              borderWidth: 0,
+              padding: 0,
+              margin: 0,
+              marginLeft: 0,
+              fontFamily: 'Nunito_400Regular',
+            }}
+          />
+          <Text style={styles.checkBoxText}>Afternoon Shift</Text>
+        </View>
+        <View style={{ marginTop: 20 }}></View>
+        <Text style={styles.mainText}>Leave Comments</Text>
+        <TextInput style={styles.comments} />
         <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Track</Text>
+          <Text style={styles.buttonText}>Apply</Text>
         </TouchableOpacity>
       </View>
-      <Footer dashboard={true} />
+      <Footer calendar={true} />
     </View>
   )
 }
@@ -63,17 +77,17 @@ export default function Leave() {
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
-    maxHeight: '100%',
     flexDirection: 'column',
     justifyContent: 'space-between',
     height: '100%',
+    maxHeight: '100%',
   },
   contentContainer: {
     display: 'flex',
     flexDirection: 'column',
     alignContent: 'space-between',
     paddingHorizontal: '5%',
-    top: 0,
+    top: -10,
     bottom: 0,
   },
   mainText: {
@@ -97,27 +111,13 @@ const styles = StyleSheet.create({
     fontFamily: 'Nunito_200ExtraLight',
     letterSpacing: 3,
     marginBottom: '20%',
-    // top: '-100%',
   },
-  ongoingBox: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    backgroundColor: 'white',
-    margin: 5,
-    height: '70%',
-
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowOpacity: 0.27,
-    shadowRadius: 4.65,
-    elevation: 6,
-    marginTop: 30,
+  comments: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: 'gray',
     borderRadius: 10,
+    height: 250,
   },
   button: {
     alignSelf: 'center',
